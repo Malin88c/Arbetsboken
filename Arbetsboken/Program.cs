39c3ef29b997dff61f612929597335115a518247
Malin88c <m.classon@hotmail.com>
2015-06-20 23:10:26 +0200

    u733

--- Arbetsboken/Program.cs
+++ Arbetsboken/Program.cs
@@ -13,10 +13,128 @@ namespace Arbetsboken
             //u712();
             //u713();
             //u714();
-            u715();
+            //u715();
+            //u716();
+            //u717();
+            //u719();
+            //u7110();
+            //u731();
+            u733();
+        }
+
+        static void u733()
+        {
+       
+            Console.WriteLine("Är det fint väder? j/n");
+            string str = Console.ReadLine();
+
+            Console.Clear();
+
+            if (str == "j")
+            {
+                Console.WriteLine("Vi går på Picknick!");
+                Console.ReadLine();
+            }
+
+            else if (str == "n")
+            {
+                Console.WriteLine("Vi stannar inne och läser en bok!");
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("Jag förstår inte.");
+                Console.ReadLine();
+            }
+        }
+        static void u732()
+        {
+
+            Console.WriteLine("Är det fint väder? j/n");
+            string str = Console.ReadLine();
+
+            Console.Clear();
+
+            if (str == "j")
+            {
+                Console.WriteLine("Vi går på Picknick!");
+                Console.ReadLine();
+            }
+
+            else if (str == "n")
+            {
+                Console.WriteLine("Vi stannar inne och läser en bok!");
+                Console.ReadLine();
+
+            }
+        }
+
+        static void u731()
+        {
+
+            Console.WriteLine("Är det fint väder?");
+            string str = Console.ReadLine();
+
+            Console.Clear();
+
+            if (str == "j") 
+            {
+                Console.WriteLine("Vi går på Picknick!");
+                Console.ReadLine();
+            }
+
 
         }
-        
+
+        static void u7110()
+        {
+
+            int a = 8 % 3;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            //Det som blir över efter division , % Modulus, Operatorn
+        }
+
+        static void u719()
+        {
+            int a = 1;
+            int b = 2;
+            decimal c = Convert.ToDecimal(a) / Convert.ToDecimal(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+            //Lösning 1 - Bytte float till decimal och converterade int till decimal.
+
+        }
+        static void u717()
+        {
+            char myChar = (char)97;
+            Console.WriteLine(myChar);
+            Console.ReadLine();
+        }
+
+        static void u716()
+        {
+            Console.Write("Vad är en tredjedel av 100? ");
+            
+
+
+            string stringTal = Console.ReadLine();
+               
+
+            float floatTal = float.Parse(stringTal);
+
+            
+            double doubleTal = Math.Round(floatTal, 2) ;
+
+            Console.Write("Avrundat till två decimaler blir talet " + doubleTal);
+            Console.ReadLine();
+
+
+
+        }
         static void u715()
         {
             Console.Write("Vad heter du? ");
